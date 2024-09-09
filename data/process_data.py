33617095ce2db17dfec9feecf2f7ac14aec24aa8
thomas-his-github <sips.thomas@gmail.com>
2024-09-09 08:46:42 +0200

    Update process_data.py
    fixed indentation error

--- data/process_data.py
+++ data/process_data.py
@@ -2,6 +2,7 @@ import sys
 import pandas as pd
 from sqlalchemy import create_engine
 
+
 def load_data(messages_filepath, categories_filepath):
     """
     Load data from messages and category files and merge into a single dataframe.
@@ -13,14 +14,15 @@ def load_data(messages_filepath, categories_filepath):
     Returns:
     df : Merged dataframe containing messages and categories.
     """
-	# read messages and categories
+    # read messages and categories
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
-	
-	# Merge to dataframe and return
+
+    # Merge to dataframe and return
     df = pd.merge(messages, categories, on='id')
 
     return df
+
 
 def clean_data(df, categories_filepath):
     """
@@ -47,11 +49,10 @@ def clean_data(df, categories_filepath):
     for column in categories:
         # Set each value to be the last character of the string
         categories[column] = categories[column].str[-1]
-
-	# Replace wrong values of '2' by '1'
-	categories[column] = categories[column].replace('2', '1')
-
-
+        
+        # Replace wrong values of '2' by '1'
+        categories[column] = categories[column].replace('2', '1')
+        
         # Convert column from string to numeric
         categories[column] = pd.to_numeric(categories[column])
 
@@ -65,6 +66,7 @@ def clean_data(df, categories_filepath):
 
     return df_clean
 
+
 def save_data(df, database_filename):
     """
     Save the cleaned dataframe to a database.
@@ -73,8 +75,9 @@ def save_data(df, database_filename):
     df : Cleaned dataframe.
     database_filename : Filepath for the database.
     """
-    engine = create_engine('sqlite:///'+ database_filename)
+    engine = create_engine('sqlite:///' + database_filename)
     df.to_sql('DisasterResponse_table', engine, index=False, if_exists='replace')
+
 
 def main():
     """
@@ -105,4 +108,4 @@ def main():
 
 
 if __name__ == '__main__':
-	main()
+    main()
